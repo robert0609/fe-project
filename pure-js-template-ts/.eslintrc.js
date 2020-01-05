ecc16607630bbd8840ffcd690a72090836bc5731
yangxu <robert0609@163.com>
2020-01-06 00:06:06 +0800

    eslint update

--- pure-js-template-ts/.eslintrc.js
+++ pure-js-template-ts/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   "plugins": ["@typescript-eslint"],
   "extends": [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended"
   ],
   "rules": {
--- server-app-template-ts/.eslintrc.js
+++ server-app-template-ts/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   "plugins": ["@typescript-eslint"],
   "extends": [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended"
   ],
   "rules": {
